f704498311d5a9fedfb3b5a9fe7273afdb3877a6
momixer1 <moritzdiego2008@outlook.de>
2026-09-14 19:56:57 +0200

    working 2-stage engine

--- engine.py
+++ engine.py
@@ -11,13 +11,23 @@ model.prompts = {"default": "passage: ",
 model.default_prompt_name = "query"
 cross_encoder = CrossEncoder('cross-encoder/mmarco-mMiniLMv2-L12-H384-v1', local_files_only=True, device=device)
 
+print("loading data...")
 books = pd.read_csv("clean_books.csv")
-num_books = len(books)
-embedding_dim = 384 #standard
+books["combined_features"] = (
+    "Title: " +
+    books["Name"] +
+    "| Author: " +
+    books["Authors"] +
+    "| Year: " +
+    books["PublishYear"].astype(str) +
+    "| Description: " +
+    books["Description"]
+)
+descriptions = books["combined_features"]
+
+print("loading embeddings...")
 embeddings = np.load("book_embeddings.npy", mmap_mode="r")
 embeddings_gpu = torch.from_numpy(embeddings).to(device)
-
-
 
 def get_results(search_query:str, top_n: int=5):
 
@@ -28,9 +38,16 @@ def get_results(search_query:str, top_n: int=5):
 
     top_scores, top_indicies = torch.topk(scores, top_n * 4)
     top_indicies = top_indicies.cpu().numpy()
-    print(top_indicies)
-    return books["Name"].iloc[top_indicies].tolist()
 
-print("Ready")
+
+    candidate_descriptions = descriptions.iloc[top_indicies].tolist()
+    raw_cross_scores = cross_encoder.predict([(search_query, desc) for desc in candidate_descriptions])
+    normalized_cross_scores = np.argsort(1/(1+ np.exp(raw_cross_scores * (-1))))
+    top_cross_indicies = normalized_cross_scores[:top_n]
+
+    final_indicies = top_indicies[top_cross_indicies]
+    return books["Name"].iloc[final_indicies].tolist()
+
+
 while True:
-    print(get_results(input(), 1))
+    print(get_results(input("What type of book are you looking for? "), 5))
