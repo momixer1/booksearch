3d0cf3daff91ce0168d45285348072fbf6c27e5b
momixer1 <moritzdiego2008@outlook.de>
2026-09-14 22:31:42 +0200

    fixed scores, cli,

--- engine.py
+++ engine.py
@@ -27,27 +27,46 @@ descriptions = books["combined_features"]
 
 print("loading embeddings...")
 embeddings = np.load("book_embeddings.npy", mmap_mode="r")
-embeddings_gpu = torch.from_numpy(embeddings).to(device)
+embeddings_gpu = torch.tensor(embeddings, device=device)
 
 def get_results(search_query:str, top_n: int=5):
 
     scores = model.similarity(
         embeddings1=embeddings_gpu,
-        embeddings2=torch.tensor(model.encode(search_query, device=device).reshape(1, -1)).to(device)
+        embeddings2=torch.tensor(model.encode(search_query, prompt="query: ",  device=device).reshape(1, -1)).to(device)
     ).squeeze()
 
-    top_scores, top_indicies = torch.topk(scores, top_n * 4)
+    top_scores, top_indicies = torch.topk(scores, top_n * 5)
     top_indicies = top_indicies.cpu().numpy()
-
 
     candidate_descriptions = descriptions.iloc[top_indicies].tolist()
     raw_cross_scores = cross_encoder.predict([(search_query, desc) for desc in candidate_descriptions])
-    normalized_cross_scores = np.argsort(1/(1+ np.exp(raw_cross_scores * (-1))))
-    top_cross_indicies = normalized_cross_scores[:top_n]
 
+    sorted_cross_scores = np.argsort(raw_cross_scores)[::-1]
+    top_cross_indicies = sorted_cross_scores[:top_n]
+    top_scores = raw_cross_scores[top_cross_indicies].tolist()
     final_indicies = top_indicies[top_cross_indicies]
-    return books["Name"].iloc[final_indicies].tolist()
+
+    recommendations = []
+    for i in range(top_n):
+        idx = final_indicies[i]
+        book_data = {
+            "rank": i+1,
+            "name": books["Name"].iloc[idx],
+            "score": round(float(sigmoid(top_scores[i]+3) * 100), 4)
+        }
+        recommendations.append(book_data)
+        
+    return recommendations
+
+def sigmoid(x):
+    x = np.clip(x, -50, 50)
+    return 1 / (1 + np.exp(-x))
 
 
 while True:
-    print(get_results(input("What type of book are you looking for? "), 5))
+    print('-' * 50)
+    results = get_results(input("What type of book are you looking for? "), 10)
+    for book in results:
+        print('-' * 50)
+        print(f"{book['rank']}: {book['name']} | score: {book['score']}%")
