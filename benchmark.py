ac7715e79a77a98b98b29800fa9abe608c6fbc55
momixer1 <moritzdiego2008@outlook.de>
2026-09-16 21:54:26 +0200

    improved a little

--- benchmark.py
+++ benchmark.py
@@ -4,7 +4,7 @@ import numpy as np
 def mrr(results:list, target: str):
     for book in results:
         # print(f"Id: {book['Id']}; Rank: {book['rank']}")
-        if target.lower() in book["name"].lower():
+        if target.lower() in book["title"].lower():
             return 1/book["rank"]
     return 0
 
@@ -22,7 +22,7 @@ queries = [["Harry Potter's first year at Hogwarts", "Harry Potter and the Sorce
             ["teenage mutants learning to control their powers at Xavier's school to become the next generation of heroes", "The New Mutants Classic"],
             ["in-depth biography of the avant-garde diarist exploring her bohemian life, relationships, and literary career", "Anaïs Nin: A Biography"],
             ["a large family with ten children moves to the country and desperately tries to convince their parents to get an animal", "Ten Kids, No Pets"],
-            ["an ethical and philosophical examination of modern factory farming and the impact of our food consumption", "The Ethics of What We Eat: Why Our Food Choices Matter"],
+            ["an ethical and philosophical examination of modern factory farming and the impact of our food consumption", "We Eat: Why Our Food Choices Matter"],
             ["young heroes embark on a dangerous fantasy quest across Avalon as a dark threat causes the stars to vanish", "Shadows on the Stars"],
             ["playful rhyming children's book introducing toddlers to whimsical pets and silly creatures", "Wet Pet, Dry Pet, Your Pet, My Pet"],
             ["memoir of a pioneer woman enduring hardships and the wagon journey westward during the California Gold Rush", "Frontier Lady"]]
@@ -31,5 +31,11 @@ queries = [["Harry Potter's first year at Hogwarts", "Harry Potter and the Sorce
 print("evaluating...")
 for i in range(len(queries)):
     mrr_score.append(mrr(get_results(queries[i][0], 10), queries[i][1]))
+    # print('-' * 50)
+    # results = get_results(queries[i][0], 10)
+    # for book in results:
+    #     print('-' * 50)
+    #     print(f"{book['rank']}: {book['name']} | score: {book['score']}%")
+
 print(mrr_score)
 print(f"MRR @ 10: {np.mean(mrr_score)}")
